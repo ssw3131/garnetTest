3e3fc6384ad40a805e1851406a4a3596f82f8f08
ssw3131 <ssw3131@daum.net>
2014-04-07 14:16:40 +0900

    ajax

--- testCase/js/testCase.js
+++ testCase/js/testCase.js
@@ -915,6 +915,7 @@ Dk.init( function(){
      * @returns {DkSpriteSheet}
      */
     function addSpriteSheet(){
+        trace( 1 );
         _setTotal();
 //		var box = Dk.dom( "sheet" ).css( "left", Dk.util.randomIntRange( _dkContainerWidth - 50 ), "top", Dk.util.randomIntRange( _dkContainerHeight - 50 ) ).tr( "addParent", _dkView )
 //			.ss( "load", "asset/box.png", "asset/box.json" ).ss("repeat");
@@ -922,7 +923,7 @@ Dk.init( function(){
         var x = Dk.util.randomIntRange( _dkContainerWidth - 400 ), y = Dk.util.randomIntRange( _dkContainerHeight - 200 );
         Dk.dom().css( "position", "absolute", "left", x, "top", y, "width", 400, "height", 200, "backgroundColor", Dk.util.randomColor(), "opacity", 0.05 ).tr( "addParent", _dkView );
         var box2 = Dk.sheet().css( "left", x, "top", y ).tr( "addParent", _dkView )
-            .ss( "load", "asset/particle.png", "asset/particle.txt", 30 );
+//            .ss( "load", "asset/particle.png", "asset/particle.txt", 30 );
 //            .ss( "load", "asset/particle.png", "asset/particle.txt", 30 ).ss( "repeat" );
         return box2;
     };
