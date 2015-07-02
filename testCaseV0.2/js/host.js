9b8cf4ba5ac283b2972995afa5dc897b5c82f878
sewon <ssw3131@daum.net>
2015-07-02 19:53:30 +0900

    dk v0.2.1

--- testCaseV0.2/js/host.js
+++ testCaseV0.2/js/host.js
@@ -2,18 +2,18 @@
  * Created by sewon on 2015-03-05.
  */
 ;
-alert( dk.WIN.width )
 dk( function(){
-	alert( dk.WIN.width )
 	// css
 	dk.Css( 'body' ).S( 'margin', 0, 'padding', 0, 'bgColor', '#eceef4', 'color', '#808081', 'font', '12px/18px Helvetica, Arial, Nanum Gothic, AppleGothic, Dotum, Sans-Serif' ),
 		dk.Css( 'a:link, a:visited, a:active' ).S( 'color', '#808081', 'textDecoration', 'none' ),
 		dk.Css( 'a:hover' ).S( 'color', '#222', 'textDecoration', 'underline' ),
-		dk.Css( '.hr2' ).S( 'margin', '30px 0', 'borderTop', '1px solid #aaa' ),
+		dk.Css( '.hr2' ).S( 'margin', '20px 0', 'borderTop', '1px solid #aaa' ),
 		dk.Css( '.hr3' ).S( 'margin', '10px 0', 'borderTop', '1px dashed #bbb' ),
 
 		dk.Css( '#container' ).S( 'margin', 30, 'padding', 30, 'maxWidth', 1200, 'minWidth', 300, 'bgColor', '#fff', 'border', '1px solid #c9c9c9', 'boxShadow', '0px 0px 8px #62615f inset', 'borderRadius', 16 ),
-		dk.Css( '#container h1' ).S( 'color', '#444' );
-		dk.Css( '#container h2' ).S( 'color', '#1a99aa' );
-		dk.Css( '#container h3' ).S( 'color', '#1a99aa', 'fontSize', 12 );
+		dk.Css( '#container h1' ).S( 'color', '#444' ),
+		dk.Css( '#container h2' ).S( 'color', '#1a99aa' ),
+		dk.Css( '#container h3' ).S( 'color', '#1a99aa', 'fontSize', 12 ),
+
+		dk.Css( '.content' ).S( 'padding', 20, 'marginBottom', 10, 'border', '1px solid #c9c9c9', 'boxShadow', '0px 0px 4px #62615f', 'borderRadius', 8 );
 } )
