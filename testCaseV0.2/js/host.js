6ec049a2ffcf26854fd56197186b9c885ac46f41
sewon <ssw3131@daum.net>
2015-07-08 18:33:49 +0900

    dk v0.2.1 pinch

--- testCaseV0.2/js/host.js
+++ testCaseV0.2/js/host.js
@@ -10,10 +10,13 @@ dk( function(){
 		dk.Css( '.hr2' ).S( 'margin', '20px 0', 'borderTop', '1px solid #aaa' ),
 		dk.Css( '.hr3' ).S( 'margin', '10px 0', 'borderTop', '1px dashed #bbb' ),
 
-		dk.Css( '#container' ).S( 'margin', 30, 'padding', 30, 'maxWidth', 1200, 'minWidth', 300, 'bgColor', '#fff', 'border', '1px solid #c9c9c9', 'boxShadow', '0px 0px 8px #62615f inset', 'borderRadius', 16 ),
+		dk.Css( '#container' ).S( 'margin', 30, 'padding', 30, 'maxWidth', 1200, 'minWidth', 250, 'bgColor', '#fff', 'border', '1px solid #c9c9c9', 'boxShadow', '0px 0px 8px #62615f inset', 'borderRadius', 16 ),
 		dk.Css( '#container h1' ).S( 'color', '#444' ),
 		dk.Css( '#container h2' ).S( 'color', '#1a99aa' ),
 		dk.Css( '#container h3' ).S( 'color', '#1a99aa', 'fontSize', 12 ),
 
 		dk.Css( '.content' ).S( 'padding', 20, 'marginBottom', 10, 'border', '1px solid #c9c9c9', 'boxShadow', '0px 0px 4px #62615f', 'borderRadius', 8 );
+
+	log.show();
+	log.position( 0, 0, 150 )
 } )
