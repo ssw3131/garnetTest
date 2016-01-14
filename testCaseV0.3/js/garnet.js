0583f1b366784772f304a15c9ae9028eb61813b0
sewon <ssw3131@daum.net>
2016-01-14 17:58:16 +0900

    dk garnet v0.3

--- testCaseV0.3/js/garnet.js
+++ testCaseV0.3/js/garnet.js
@@ -17,5 +17,63 @@
 			};
 		} ),
 
-		log( dk );
+// dk :
+		W.dk = W[ 'dk' ] ? W[ 'dk' ] : dk = (function( $doc ){
+			return function( $host ){
+				var check;
+				check = setInterval( function(){
+					switch( $doc.readyState ){
+						case'complete':
+						case'interactive':
+						case'loaded':
+							break;
+						default:
+							return
+					}
+					if( $doc && $doc.getElementsByTagName && $doc.getElementById && $doc.body ){
+						clearInterval( check ), $host ? $host() : null;
+					}
+				}, 10 );
+			}
+		})( DOC ),
+
+// CORE :
+		dk.fn = function( $k, $v ){
+			$k = $k.replace( trim, '' ), $k = $k.charAt( 0 ).toLowerCase() + $k.substring( 1, $k.length ),
+				dk[ $k ] ? dk.err( 'dk.fn에 이미 ' + $k + '값이 존재합니다' ) : dk[ $k ] = $v;
+		},
+		dk.cls = function( $k, $v ){
+			$k = $k.replace( trim, '' ), $k = $k.charAt( 0 ).toUpperCase() + $k.substring( 1, $k.length ),
+				dk[ $k ] ? dk.err( 'dk.cls에 이미 ' + $k + '값이 존재합니다' ) : dk[ $k ] = $v;
+		},
+		dk.obj = function( $k, $v ){
+			$k = $k.replace( trim, '' ).toUpperCase(),
+				dk[ $k ] ? dk.err( 'dk.obj에 이미 ' + $k + '값이 존재합니다' ) : dk[ $k ] = $v;
+		},
+
+// INFO :
+		dk.obj( 'INFO', { name : 'Dk garnet', version : 'v0.3.1', github : 'https://github.com/ssw3131/garnet.git' } ),
+
+// ERROR :
+		dk.fn( 'err', function( $log ){ log( 'err : ' + $log ); } ),
+// DETECTOR :
+		// todo detector update
+		dk.obj( 'DETECTOR', (function( $w, $doc ){
+			var navi = $w.navigator, agent = navi.userAgent.toLowerCase(), platform = navi.platform.toLowerCase(), app = navi.appVersion.toLowerCase(),
+				device = 'pc', os, osv, browser, bv, flash,
+				prefixCss, prefixStyle, transform3D, keyframe = $w[ 'CSSRule' ],
+				docMode = 0,
+				d = $doc.createElement( 'div' ), s = d.style, c = $doc.createElement( 'canvas' ), a = $doc.createElement( 'audio' ), v = $doc.createElement( 'video' ), t0,
+				ie, chrome, firefox, safari, opera, naver;
+
+			log( navi );
+			log( agent );
+			log( platform );
+			log( app );
+
+			return {
+			}
+		})( W, DOC ) ),
+
+		log( 'code end' );
 })();
