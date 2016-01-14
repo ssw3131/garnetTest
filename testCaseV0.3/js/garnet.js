21076a4cfda1fe2f3d6bcbcbc4a55afd7437a498
sewon <ssw3131@daum.net>
2016-01-14 17:14:43 +0900

    dk garnet v0.3

--- testCaseV0.3/js/garnet.js
+++ testCaseV0.3/js/garnet.js
@@ -10,12 +10,12 @@
 		W.log = W[ 'log' ] ? W[ 'log' ] : function(){ W.console.log( arguments[ 0 ] ) },
 		Date.now = Date.now * 1 || function(){ return +new Date },
 		W.requestAnimFrame = (function(){ return W.requestAnimationFrame || W.webkitRequestAnimationFrame || W.mozRequestAnimationFrame || function( $loop ){ W.setTimeout( $loop, 17 ) } })(),
-		/*(function( f ){ W.setTimeout = f( W.setTimeout ), W.setInterval = f( W.setInterval ) })( function( f ){
+		(function( f ){ W.setTimeout = f( W.setTimeout ), W.setInterval = f( W.setInterval ) })( function( f ){
 			return function( $a, $b ){
 				var a = [].slice.call( arguments, 2 );
 				return f( function(){ $a.apply( this, a ); }, $b );
 			};
-		} ),*/
+		} ),
 
 		log( dk );
 })();
