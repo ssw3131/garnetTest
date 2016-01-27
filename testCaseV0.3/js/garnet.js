b640f77eff1726987868e4a085d2c36fb5e39b3c
sewon <ssw3131@daum.net>
2016-01-27 12:22:47 +0900

    dk garnet v0.3
    DOM

--- testCaseV0.3/js/garnet.js
+++ testCaseV0.3/js/garnet.js
@@ -645,6 +645,18 @@
 				}
 				$fn.apply( undefined, param );
 			},
+			attr : (function(){
+				return {
+					'@addClass' : function( $v ){
+						var e = this.el;
+						e.setAttribute( 'class', e.getAttribute( 'class' ) + ' ' + $v );
+					},
+					'@delClass' : function( $v ){
+						var e = this.el;
+						e.setAttribute( 'class', e.getAttribute( 'class' ).replace( $v, '' ) );
+					}
+				}
+			})(),
 			css : (function( $detector ){
 				var prefixCss = $detector.prefixCss, dtFloat = $detector.float, t0 = $detector.ie8;
 				return {
@@ -797,7 +809,50 @@
 
 			return factory;
 		})( DOC, dk.selector, dk.DETECTOR ) ),
-		dk.PROTO.connect( dk.Dom.fn, dk.PROTO.css, dk.PROTO.tree, dk.PROTO.event ),
+		dk.PROTO.connect( dk.Dom.fn, dk.PROTO.attr, dk.PROTO.css, dk.PROTO.tree, dk.PROTO.event ),
+
+// CSS :
+		dk.cls( 'Css', (function( $doc, $head, $detector ){
+			var factory, Css, uuList = {}, proto = {}, el, sheet, rules;
+			el = $doc.createElement( 'style' ), $head.appendChild( el ), sheet = el.sheet || el.styleSheet, rules = sheet.cssRules || sheet.rules,
+
+				Css = sheet.insertRule ? function( $key ){
+					this.sheet = sheet, this.rules = rules, this.cssId = rules.length, sheet.insertRule( $key + '{}', this.cssId );
+				} : sheet.addRule ? function( $key ){
+					this.sheet = sheet, this.rules = rules, this.cssId = rules.length, sheet.addRule( $key, ' ', this.cssId );
+				} : function( $key ){
+					dk.err( 'sheet에 rule을 추가할 수 없습니다.' );
+				},
+				Css.prototype.S = (function(){
+					var prefixCss = $detector.prefixCss, nopx = { zIndex : 1, 'z-index' : 1 };
+					return function(){
+						var i = 0, j = arguments.length, k, v, s = this.rules[ this.cssId ].style, r, t0;
+						while( i < j ){
+							k = arguments[ i++ ];
+							if( i == j ) return proto[ k ] ? proto[ k ].call( { style : s } ) :
+								( r = s[ k ], t0 = parseFloat( r ), r = isNaN( t0 ) ? r : t0 );
+							else  v = arguments[ i++ ],
+								proto[ k ] ? proto[ k ].call( { style : s }, v ) :
+									s[ k ] = s[ prefixCss + k ] = typeof v == 'number' ? nopx[ k ] ? v : v + 'px' : v
+						}
+						return this;
+					}
+				})(),
+
+				factory = function( $k ){
+					return uuList[ $k ] ? uuList[ $k ] : uuList[ $k ] = new Css( $k );
+				},
+				factory.fn = function(){
+					var i = 0, j = arguments.length, k, v;
+					while( i < j ){
+						k = arguments[ i++ ];
+						if( i == j ) return proto[ k ];
+						else v = arguments[ i++ ], v === null ? delete proto[ k ] : proto[ k ] = v;
+					}
+				};
+			return factory;
+		})( DOC, HEAD, dk.DETECTOR ) ),
+		dk.PROTO.connect( dk.Css.fn, dk.PROTO.css ),
 
 		log( 'code end' );
 })();
